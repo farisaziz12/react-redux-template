1c6adb50c8ee56d80b499e5e1211625045e68675
Faris Aziz <farisaziz12@gmail.com>
2021-06-12 22:31:53 +0200

    feat(index): pipe through bootstrap funcs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { of } from "rxjs";
+import { take, map } from "rxjs/operators";
+import App from "./App";
+import { bootstrapReduxStore } from "./BootstrapFunctions";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+of(<App />)
+  .pipe(take(1))
+  .pipe(map(bootstrapReduxStore))
+  .subscribe((app) => {
+    ReactDOM.render(
+      <React.StrictMode>{app}</React.StrictMode>,
+      document.getElementById("root")
+    );
+  });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

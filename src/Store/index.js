02f223653bf52c5af240119dfd65062d4942eef5
Faris Aziz <farisaziz12@gmail.com>
2021-06-13 16:15:28 +0200

    feat(store): enable dev tools

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,4 +1,7 @@
 import { createStore } from "redux";
 import { rootReducer } from "./Reducers";
 
-export const store = createStore(rootReducer);
+export const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // remove for PROD
+);

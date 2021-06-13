03d9d40a92878e4c0cc795c46dac9422bb8b42d0
Faris Aziz <farisaziz12@gmail.com>
2021-06-13 16:16:31 +0200

    feat(user-reducer): add init state and update

--- src/Store/Reducers/user/userReducer.js
+++ src/Store/Reducers/user/userReducer.js
@@ -1,9 +1,15 @@
-const initialState = {};
+import { USER } from "./userActions";
+
+const initialState = {
+  firstName: "",
+  lastName: "",
+};
 
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "": {
-      return {};
+    case USER.UPDATE: {
+      const { payload } = action;
+      return { ...state, ...payload };
     }
     default:
       return state;

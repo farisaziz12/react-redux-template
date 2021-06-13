c089506fe92be322b9253bcf48375d7e46ea8677
Faris Aziz <farisaziz12@gmail.com>
2021-06-13 16:15:51 +0200

    feat(reducers): create actions obj

--- src/Store/Reducers/index.js
+++ src/Store/Reducers/index.js
@@ -1,4 +1,11 @@
 import { combineReducers } from "redux";
+// Reducers
 import { userReducer } from "./user";
+// Actions
+import { USER } from "./user/userActions";
 
 export const rootReducer = combineReducers({ user: userReducer });
+
+export const ACTIONS = {
+  USER,
+};
